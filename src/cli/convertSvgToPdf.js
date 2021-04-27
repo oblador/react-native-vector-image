86dd1b35cf77636cb6155cf272121dbc1fcff691
Joel Arvidsson <joel@oblador.se>
2021-04-27 12:02:35 +0200

    Make PDF creation deterministic (#6)

--- src/cli/convertSvgToPdf.js
+++ src/cli/convertSvgToPdf.js
@@ -4,7 +4,17 @@ const SVGtoPDF = require('svg-to-pdfkit');
 
 function convertSvgToPdf(sourcePath, targetPath, size) {
   return new Promise((resolve, reject) => {
-    const doc = new PDFDocument({ size, margin: 0 });
+    const doc = new PDFDocument({
+      size,
+      margin: 0,
+      info: {
+        // To make builds deterministic we need to set a static
+        // creation date (and file descriptors are not content
+        // reliable in this regard)
+        // https://www.nasa.gov/mission_pages/apollo/apollo11.html
+        CreationDate: new Date('July 20, 69 00:20:18 GMT+00:00'),
+      },
+    });
     const stream = fs.createWriteStream(targetPath);
     stream.on('finish', resolve);
     stream.on('error', reject);
