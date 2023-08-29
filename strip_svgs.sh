ea8f610de42a97537d82e340d4b8c4e220f5d15b
Artem Mizinin <mizinin@users.noreply.github.com>
2023-08-29 20:29:13 +0700

    Put RN_ASSETS_PATH in quotes (#42)

--- strip_svgs.sh
+++ strip_svgs.sh
@@ -6,5 +6,5 @@
 RN_ASSETS_PATH="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH/assets"
 
 if [ -d "$RN_ASSETS_PATH" ]; then
-  find $RN_ASSETS_PATH -type f -name "*.svg" -delete 
+  find "$RN_ASSETS_PATH" -type f -name "*.svg" -delete 
 fi
