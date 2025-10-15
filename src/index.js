b30a612f4730510efd19d2a067bac85d2b0bb63e
Joel Arvidsson <joel@oblador.se>
2025-10-15 12:34:42 +0200

    Add Image.resolveAssetSource (#49)

--- src/index.js
+++ src/index.js
@@ -3,20 +3,27 @@ import { Image } from 'react-native';
 import { getAssetByID } from '@react-native/assets-registry/registry';
 import getResourceName from './getResourceName';
 
-export default function VectorImage({ source, style, ...props }) {
+export function resolveVectorAssetSource(source) {
   const asset = getAssetByID(source);
+  if (!asset) {
+    return null;
+  }
+  const resourceName = getResourceName(asset);
+  return { uri: resourceName,width: asset.width, height: asset.height }
+}
+
+export default function VectorImage({ source, ...props }) {
+  const asset = resolveVectorAssetSource(source);
   if (!asset) {
     console.warn(
       `No asset registered for source "${source}", you may have to generate assets and recompile`
     );
     return null;
   }
-  const resourceName = getResourceName(asset);
+
   return (
-    <Image
-      source={{ uri: resourceName }}
-      style={[{ width: asset.width, height: asset.height }, style]}
-      {...props}
-    />
+    <Image source={asset} {...props} />
   );
 }
+
+VectorImage.resolveAssetSource = resolveVectorAssetSource;
